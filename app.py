5b9b884746bf7e5bf2f70f0eba3bd52b8a4ead37
soumyapa <so_poumya@hotmail.com>
2020-01-28 12:12:42 +0530

    Formatting Updates

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ import sys
 from flask import Flask
 from flask import render_template , url_for,request , redirect
 import flask
+from flask_paginate import Pagination , get_page_parameter , get_page_args
 
 
 #-------- MODEL GOES HERE -----------#
 import pandas as pd
 import numpy as np
-from sklearn.cluster import KMeans
 from sklearn.feature_extraction.text import TfidfVectorizer , CountVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 from sklearn.metrics.pairwise import cosine_similarity
@@ -52,25 +52,45 @@ def get_recommendations(title, cosine_sim=cosine_sim):
     # Get the books indices
     book_indices = [i[0] for i in sim_scores]
     t_h = DF['title'].iloc[book_indices]
-    print(type(DF.loc[DF['title'] == t_h.values[0], ['authors']].values[0].tolist()[0]))
+    
     print(DF.loc[DF['title'] == t_h.values[0], ['authors']].values[0].tolist()[0])
     # Return the top 5 most similar movies
     return t_h
 
+def get_books(df , offset=0, per_page=100) :
+    return df[offset: offset + per_page]
 
+
+@app.route('/')
+def index():
+    q = DF['title'].sort_values()
+    page, per_page, offset = get_page_args(page_parameter='page',
+                                        per_page_parameter='per_page')
+    total = q.count()
     
+    pagination_titles = get_books(q , offset=offset, per_page=per_page)
+    pagination = Pagination(page=page, per_page=per_page, total=total,
+                            css_framework='bootstrap4')
+   
+    # 'page' is the default name of the page parameter, it can be customized
+    # e.g. Pagination(page_parameter='p', ...)
+    # or set PAGE_PARAMETER in config file
+    # also likes page_parameter, you can customize for per_page_parameter
+    # you can set PER_PAGE_PARAMETER in config file
+    # e.g. Pagination(per_page_parameter='pp')
 
+    return render_template('title.html',
+                           titles=pagination_titles,
+                           page=page,
+                           per_page=per_page,
+                           pagination=pagination,
+                           )
 @app.route('/result', methods=['POST', 'GET'])
 def result():
     '''Gets prediction using the HTML form'''
     if flask.request.method == 'GET':
         words = flask.request.args['words']
-        return render_template('book_reco_result.html', results = get_recommendations(words),input_words = words , df = DF)
-
-
-@app.route('/')
-def index():
-    return render_template('page.html' , table_data_title = DF['title'],table_data_author = DF['authors'],table_data_review = DF['review'])
+        return render_template('book_reco_result.html', results = get_recommendations(words), input_words = words , df = DF)
 
 if __name__ == '__main__':
     app.run(debug=True)
